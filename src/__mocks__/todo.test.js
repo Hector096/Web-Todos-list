52652b6d4e683eba8badd25792391f28e25d5d89
Hector <vv96rca@gmail.com>
2021-10-01 19:56:35 +0530

    testing completed and updation of description

--- src/__mocks__/todo.test.js
+++ src/__mocks__/todo.test.js
@@ -1,4 +1,5 @@
 import Task from '../dataModel';
+import { isCompleted, clearCompleted, isEdit } from '../isCompleted';
 
 class LocalStorageMock {
   constructor() {
@@ -25,7 +26,7 @@ class LocalStorageMock {
 global.localStorage = new LocalStorageMock();
 
 describe('Add item and Remove item', () => {
-  test('add item to the localstorage', () => {
+  test('add item to the list', () => {
     const data = JSON.parse(localStorage.getItem('Tasks')) || [];
     const task1 = new Task('to buy a new Phone', data.length + 1, false);
     task1.addNewTask();
@@ -39,6 +40,13 @@ describe('Add item and Remove item', () => {
     expect(dataAfterAdding.length).toBe(3);
   });
 
+  test('updation of description', () => {
+    isEdit(1, 'To buy Honey');
+    const data = JSON.parse(localStorage.getItem('Tasks')) || [];
+    const index = data.findIndex((item) => item.index === 1);
+    expect(data[index].description).toBe('To buy Honey');
+  });
+
   test('remove item to the list', () => {
     const task = new Task('to buy a new Phone', 1, false);
     task.removeTask();
@@ -46,10 +54,26 @@ describe('Add item and Remove item', () => {
     expect(data.length).toBe(2);
   });
 
-  test('Checking the index value after removing the item', () => {
+  test('Checking the index value after removing the item from the list', () => {
     const task = new Task('to buy a new Phone', 1, false);
     task.removeTask();
     const data = JSON.parse(localStorage.getItem('Tasks')) || [];
     expect(data[0].index).toBe(1);
+  });
+});
+
+describe('update completed value and clear completed from the list', () => {
+  test('checking updation of isCompleted value', () => {
+    const data = JSON.parse(localStorage.getItem('Tasks')) || [];
+    const { index } = data[0];
+    isCompleted(index);
+    const dataAfterUpdation = JSON.parse(localStorage.getItem('Tasks'));
+    expect(dataAfterUpdation[0].completed).toBe(true);
+  });
+
+  test('Clear the list with all completed task', () => {
+    clearCompleted();
+    const data = JSON.parse(localStorage.getItem('Tasks')) || [];
+    expect(data.length).toBe(0);
   });
 });
